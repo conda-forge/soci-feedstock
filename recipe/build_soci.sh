9159743d5850dc23493829ee813abde470e4883a
Johan Mabille <johan.mabille@gmail.com>
2026-06-16 11:36:54 +0200

    Enables required backend by output

--- recipe/build_soci.sh
+++ recipe/build_soci.sh
@@ -6,6 +6,29 @@ mkdir build; cd build
 
 export CTEST_OUTPUT_ON_FAILURE=1
 
+CMAKE_BACKEND_ARGS=(
+  -DSOCI_EMPTY=OFF
+  -DSOCI_DB2=OFF
+  -DSOCI_FIREBIRD=OFF
+  -DSOCI_MYSQL=OFF
+  -DSOCI_ODBC=OFF
+  -DSOCI_ORACLE=OFF
+  -DSOCI_POSTGRESQL=OFF
+  -DSOCI_SQLITE3=OFF
+)
+
+case "$PKG_NAME" in
+  soci-sqlite)
+    CMAKE_BACKEND_ARGS+=(-DSOCI_SQLITE3=ON)
+    ;;
+  soci-mysql)
+    CMAKE_BACKEND_ARGS+=(-DSOCI_MYSQL=ON)
+    ;;
+  soci-postgresql)
+    CMAKE_BACKEND_ARGS+=(-DSOCI_POSTGRESQL=ON)
+    ;;
+esac
+
 cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_PREFIX_PATH=$PREFIX    \
       -G "Unix Makefiles"            \
@@ -15,6 +38,7 @@ cmake ${CMAKE_ARGS} -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DCMAKE_BUILD_TYPE=Release     \
       -DCMAKE_INSTALL_LIBDIR=lib     \
       -DSOCI_STATIC=OFF              \
+      "${CMAKE_BACKEND_ARGS[@]}"     \
       $SRC_DIR
 
 make -j${CPU_COUNT}
